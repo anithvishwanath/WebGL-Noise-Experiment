a02047764abf2e9efb10607cc641cb6d403392be
Anith Vishwanath <11884278+anithvishwanath@users.noreply.github.com>
2024-06-05 11:40:45 -0400

    Changes to the zoom effect

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import fragment from './shaders/fragment.glsl';
 import vertex from './shaders/vertex.glsl';
 import testTexture from './textures/texture.jpg';
 import * as dat from './libraries/dat.gui.min.js';
+import gsap from 'gsap';
 
 export default class Sketch {
 	constructor(options) {
@@ -14,7 +15,7 @@ export default class Sketch {
 		this.camera = new THREE.PerspectiveCamera(30, this.width / this.height, 10, 1000);
 		this.camera.position.z = 600;
 
-		this.camera.fov = 2 * (Math.atan( (this.height / 2) / 600 ) * (180 / Math.PI)); // Calculate FOV and convert to degrees.
+		this.camera.fov = 2 * (Math.atan((this.height / 2) / 600) * (180 / Math.PI)); // Calculate FOV and convert to degrees.
 
 		this.scene = new THREE.Scene();
 
@@ -61,26 +62,35 @@ export default class Sketch {
 			wireframe: false,
 			uniforms: {
 				time: { value: 1.0 },
-				uProgress: { value: 1.0 },
+				uProgress: { value: 0.0 },
 				uTexture: { value: new THREE.TextureLoader().load(testTexture) },
 				uTextureSize: { value: new THREE.Vector2(100, 100) }, // set size of the texture
+				uCorners: { value: new THREE.Vector4(0, 0, 0, 0) },
 				uResolution: { value: new THREE.Vector2(this.width, this.height) }, //calculate screen resolution
 				uQuadSize: { value: new THREE.Vector2(300, 300) } // Size of the mesh (hardcoding plane geometry in this case)
 			},
 			vertexShader: vertex,
 			fragmentShader: fragment,
 		});
+
+		this.tl = gsap.timeline()
+			.to(this.material.uniforms.uCorners.value, { x: 1, duration: 1 })
+			.to(this.material.uniforms.uCorners.value, { y: 1, duration: 1 }, 0.1)
+			.to(this.material.uniforms.uCorners.value, { z: 1, duration: 1 }, 0.2)
+			.to(this.material.uniforms.uCorners.value, { w: 1, duration: 1 }, 0.3);
+
+
 		this.mesh = new THREE.Mesh(this.geometry, this.material);
 		this.scene.add(this.mesh);
 
 		this.mesh.position.x = 300;
-		this.mesh.rotation.z = 0.5;
 	}
 
 	render() {
 		this.time += 0.05;
 		this.material.uniforms.time.value = this.time;
-		this.material.uniforms.uProgress.value = this.settings.progress;
+		// this.material.uniforms.uProgress.value = this.settings.progress;
+		this.tl.progress(this.settings.progress);
 		this.mesh.rotation.x = this.time / 2000;
 		this.mesh.rotation.y = this.time / 1000;
 
--- src/shaders/vertex.glsl
+++ src/shaders/vertex.glsl
@@ -2,6 +2,7 @@ uniform float time;
 uniform float uProgress;
 uniform vec2 uResolution;
 uniform vec2 uQuadSize;
+uniform vec4 uCorners;
 varying vec2 vTexSize;
 
 varying vec2 vUv;
@@ -13,8 +14,14 @@ void main() {
     fullScreenState.x *= uResolution.x / uQuadSize.x;
     fullScreenState.y *= uResolution.y / uQuadSize.y;
 
+    float cornersProgress = mix(
+        mix(uCorners.z, uCorners.w, uv.x), 
+        mix(uCorners.x, uCorners.y, uv.x),
+        uv.y
+        );
+
     /* Interpolate between defaultState and fullScreenState */
-    vec4 finalState = mix(defaultState, fullScreenState, uProgress);
+    vec4 finalState = mix(defaultState, fullScreenState, cornersProgress);
 
     vTexSize = mix(uQuadSize, uResolution, uProgress); // get size of the texture used in the quad in each step of the progress animation
 
